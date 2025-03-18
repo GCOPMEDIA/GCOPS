9cbf047c8b07b9bf81d5ef01f74bead5ad937e41
Kobby24 <geffah005@st.ug.edu.gh>
2025-03-18 01:22:45 +0000

    setup

--- USSD_APP/views.py
+++ USSD_APP/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-# Create your views here.
-
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from .mtn_auth import get_mtn_access_token
 
 @csrf_exempt
 def ussd_callback(request):
@@ -18,11 +14,11 @@ def ussd_callback(request):
         if text == "":
             response = "CON Welcome to My Service\n1. Check Balance\n2. Buy Data"
         elif text == "1":
-            response = "END Your balance is $10"
+            # Call MTN API for balance
+            token = get_mtn_access_token()
+            response = "END Your balance is $10" if token else "END Error retrieving balance"
         elif text == "2":
             response = "CON Choose a data plan\n1. 1GB - $5\n2. 2GB - $10"
-        elif text == "2*1":
-            response = "END You have purchased 1GB for $5"
         else:
             response = "END Invalid option"
 
