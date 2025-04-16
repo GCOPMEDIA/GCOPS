662e84573fa58f12a3b4ff1bae75c28b95b11bf4
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-16 16:54:08 +0000

    setup underway

--- app/mainApp/models.py
+++ app/mainApp/models.py
@@ -42,11 +42,21 @@ class AuthPermission(models.Model):
         unique_together = (('content_type', 'codename'),)
 
 
-class AuthUser(AbstractUser):
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
 
     class Meta:
-        managed = True
-        db_table = 'Teachers'
+        managed = False
+        db_table = 'auth_user'
 
 
 class AuthUserGroups(models.Model):
