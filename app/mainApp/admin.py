58bddc87bc87598fa350f04b161cce914bfd029c
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-16 08:25:12 +0000

    setup underway

--- app/mainApp/admin.py
+++ app/mainApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(AuthUser)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Term)
+admin.site.register(ClassName)
