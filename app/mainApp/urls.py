1dda6e351237f8343f7c4892e1a724f52e0d5a3e
Kobby24 <geffah005@st.ug.edu.gh>
2025-04-16 12:57:59 +0000

    setup underway

--- app/mainApp/urls.py
+++ app/mainApp/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('',login_,name="login_"),
     path('teacher_view/',teacher_view,name="teacher_view"),
     path("add-student/",add_student,name="add_student"),
-    path("class-view/",class_view,name="class_view")
+    path("class-view/",class_view,name="class_view"),
+    path('add-grade/<int:student_id>/', add_grade, name='add_grade'),
+    path('view_grade/<int:student_id>/',view_grade,name='view_grade'),
+path('edit-grade/<int:grade_id>/', edit_single_grade, name='edit_single_grade'),
+
+
 
 ]
